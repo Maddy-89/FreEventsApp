7722ec7222f9e59b3044b2fc5f1f1f7b930f00d4
Madelynn <maddx@live.com.au>
2019-09-14 10:20:56 +0800

    add custom user register

--- eventFinderProject/settings.py
+++ eventFinderProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 AUTH_USER_MODEL = 'users.CustomUser'
 
@@ -124,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
