ecd046adbf2652acded7dea231680e893ea8d261
Madelynn <maddx@live.com.au>
2019-09-16 20:16:44 +0800

    move location of nav and index page so styles are used across all pages. Fix URL for login and logout

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-
+from django.urls import include, path
 
 app_name = 'users'
 
 urlpatterns = [
+path('event-finder/', include('eventFinderApp.urls')),
 path('register/', views.Register.as_view(), name='register'),
 
 ]
